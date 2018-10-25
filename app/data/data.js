e93ba68431f38753f7e0eb49b6b5641a0811613b
nfgrawker <nfgrawker@gmail.com>
2018-10-25 09:03:31 -0500

    algorith rework
    modal needs fixing

---
+++ app/data/data.js
@@ -0,0 +1,165 @@
+friendsList = [{
+    name: "Ahmed",
+    photo : "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+    scores : [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+    ]
+},
+    {
+        name: "Jacob Deming",
+        photo : "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+        scores : [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+    },
+    {
+        name: "Jeremiah Scanlon",
+        photo : "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+        scores : [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+        ]
+    },
+    {
+        name: "Louis T. Delia",
+        photo : "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+        scores : [
+            "3",
+            "3",
+            "4",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2",
+            "2",
+            "3"
+        ]
+    },
+    {
+        name: "Lou Ritter",
+        photo : "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+        scores : [
+            "4",
+            "3",
+            "4",
+            "1",
+            "5",
+            "2",
+            "5",
+            "3",
+            "1",
+            "4"
+        ]
+    },
+    {
+        name: "Jordan Biason",
+        photo : "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+        scores : [
+            "4",
+            "4",
+            "2",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "5"
+        ]
+    },
+    {
+        name: "George Gerswhin",
+        photo : "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE5NTU2MzE2MjI4OTc4MTg3/george-gershwin-9309643-1-402.jpg",
+        scores : [
+            "5",
+            "1",
+            "5",
+            "2",
+            "2",
+            "1",
+            "3",
+            "1",
+            "2",
+            "5"
+        ]
+    },
+    {
+        name: "sada",
+        photo : "sadad",
+        scores : [
+            "2",
+            "2",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3"
+        ]
+    },
+    {
+        name: "a",
+        photo : "a",
+        scores : [
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1"
+        ]
+    },
+    {
+        name: "brenda pending",
+        photo : "kjhghj",
+        scores : [
+            "3",
+            "4",
+            "1",
+            "3",
+            "2",
+            "3",
+            "4",
+            "1",
+            "3",
+            "4"
+        ]
+    }]
+
+
+
+
+module.exports = friendsList
--- server.js
+++ server.js
@@ -1,20 +1,42 @@
 var express = require("express");
 var mysql = require("mysql");
 var path = require("path");
+var friendsList = require("./app/data/data.js");
 // Create express app instance.
 var app = express();
-
+var bodyParser = require("body-parser");
 // Set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 8080;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
 
 
 app.get("/survey", function(req,res){
     res.sendFile(path.join(__dirname, "/app/public/survey.html"))
-  })
+  });
 app.post("/results", function(req, res){
-  console.log(req)
-})
+    let total = 10000;
+    for(let i in friendsList){
+        console.log(friendsList[i].scores[0])
+        var diff = 0;
+        for(let j in friendsList[i]["scores"]){
+        diff += Math.abs((parseInt(friendsList[i].scores[j])-parseInt(req.body.scores[j])))
+
+        }
+        if(diff < total){
+            total = diff
+
+        }
+        console.log(diff)
+        console.log(total)
+        var bestMatch = friendsList[i].name
+    }
+
+    res.send(bestMatch)
+});
 
 
 app.listen(PORT, function() {
